d638769f884b40901393f4614bdfe7f054392264
jhun91 <fonercis@naver.com>
2021-02-13 03:54:53 +0900

    Initial commit

---
+++ src/main/kotlin/com/tistory/jhun/jpabykotlin/JpabykotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.tistory.jhun.jpabykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpabykotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpabykotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/tistory/jhun/jpabykotlin/JpabykotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.jhun.jpabykotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpabykotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
